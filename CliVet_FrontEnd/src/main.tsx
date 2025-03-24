8ec4f85c173704bd89c5872a74ec4710c7de3574
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2025-03-24 12:03:40 -0500

    Se configura para usar React Router y se crea la base de la LandingPage

--- CliVet_FrontEnd/src/main.tsx
+++ CliVet_FrontEnd/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import './index.css';
+import Router from './router.tsx';
+
+const queryClient = new QueryClient(); // Se crea una instancia de QueryClient
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <Router />
+            <ReactQueryDevtools/>
+        </QueryClientProvider>
+    </StrictMode>
+);
---
+++ CliVet_FrontEnd/src/views/LandingPage.tsx
@@ -0,0 +1,8 @@
+
+export default function LandingPage() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold">Hola Mundo</h1>
+        </div>
+    );
+};
