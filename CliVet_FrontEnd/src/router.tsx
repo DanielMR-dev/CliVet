ee4e5764fca812d8b3eec13a1399074a61ec118b
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2025-03-25 21:43:53 -0500

    Se crea la ruta para el AdminView

--- CliVet_FrontEnd/src/router.tsx
+++ CliVet_FrontEnd/src/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import LoginView from "@/views/auth/LoginView";
 import RegisterView from "@/views/auth/RegisterView";
+import AdminView from "@/views/admin/AdminView";
 
 export default function Router() {
     return (
@@ -10,6 +11,9 @@ export default function Router() {
                 <Route path="/" element={<LandingPage/>}/> 
                 <Route path="/auth/login" element={<LoginView/>}/> 
                 <Route path="/auth/register" element={<RegisterView/>}/> 
+                <Route path="/admin" element={<AdminView/>}/>
+                <Route path="/doctor" element={<AdminView/>}/>
+                <Route path="/client" element={<AdminView/>}/>
             </Routes>
         </BrowserRouter>
     
---
+++ CliVet_FrontEnd/src/views/admin/AdminView.tsx
@@ -0,0 +1,8 @@
+
+export default function AdminView() {
+    return (
+        <div>
+            <h1>Vista Admin</h1>
+        </div>
+    )
+};
