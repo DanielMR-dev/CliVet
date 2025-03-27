059591c040ed7eff80c001125dc2e264ec90219b
Raul Velez <r.velez@utp.edu.co>
2025-03-27 15:20:45 -0500

    Se añade segunda carpeta de servicios hechos en FastAPI del modulo administrador

---
+++ Servicios/administrador/crear_colaborador.py
@@ -0,0 +1,91 @@
+from fastapi import FastAPI, Request
+from sqlalchemy import create_engine, MetaData, Table, insert, select
+
+'''
+CODIGO ENFOCADO AL SERVICIO DE CREAR COLABORADORES
+
+FUNCIONALIDADES
+    1. Recibir un JSON con la información de un nuevo colaborador y añadirlo a base de datos
+    2. Recibir un JSON con las credenciales de un médico para añadirlo a base de datos
+
+'''
+
+# Conexión a la base de datos (ajustar URL)
+DATABASE_URL = "postgresql://postgres:admin@localhost:5432/clivet"
+engine = create_engine(DATABASE_URL)
+
+# Cargar metadatos sin definir modelos
+metadata = MetaData()
+metadata.reflect(bind=engine)  # Reflejar todas las tablas existentes
+
+app = FastAPI()
+
+@app.post("/crear_colaborador")
+async def crear_colaborador(request: Request):
+    try:
+        # Se selecciona la tabla de colaboradores
+        colaboradores = Table("colaborador", metadata, autoload_with=engine)
+
+        # Recuperar los datos como un json
+        data = await request.json()
+
+        # Crear la sentencia insert
+        query = insert(colaboradores).values(
+            id=data.get("id"),
+            nombre_completo=data.get("nombre_completo"),
+            id_tipo=data.get("id_tipo"),
+            email=data.get("email"),
+            telefono=data.get("telefono"),
+            direccion=data.get("direccion")
+        )
+
+        # Ejecutar la consulta
+        with engine.connect() as connection:
+            connection.execute(query)
+            connection.commit()
+
+        return {"mensaje": "Colaborador registrado correctamente"} # Cambiar 
+
+    except Exception as e:
+        return {"error": f"Error al registrar al colaborador: {str(e)}"} # Cambiar 
+
+
+
+@app.post("/crear_medico")
+async def crear_medico(request: Request):
+    try:
+        # Seleccionar la tabla de empleados
+        empleados = Table("empleados", metadata, autoload_with=engine)
+
+        # Recuperar los datos como un json
+        data = await request.json()
+
+        # Crear la sentencia insert
+        query = insert(empleados).values(
+            id=data.get("id"),
+            nombre_completo=data.get("nombre_completo"),
+            clave=data.get("clave").encode(),
+            id_rol=data.get("id_rol")
+        )
+
+        # Ejecutar la consulta
+        with engine.connect() as connection:
+            connection.execute(query)
+            connection.commit()
+
+        return {"mensaje": "Medico registrada correctamente"} # Cambiar 
+
+    except Exception as e:
+        return {"error": f"Error al registrar al medico: {str(e)}"} # Cambiar 
+    
+
+@app.get("/medico/{id}")
+async def get_medico(id: int):
+    empleados = Table("empleados", metadata, autoload_with=engine)
+    query = select(empleados).where(empleados.c.id == id)
+    with engine.connect() as connection:
+        result = connection.execute(query)
+        rows = result.fetchall()
+
+    return [dict(row._mapping) for row in rows]
+    
---
+++ Servicios/administrador/listar_procesos.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from sqlalchemy import create_engine, MetaData, Table, select
+
+'''
+CODIGO ENFOCADO AL SERVICIO DE LISTAR PROCESOS
+
+FUNCIONALIDADES
+    1. Retornar un JSON con todos los procesos
+    2. Retornar un JSON con la informacion de un proceso en especifico
+
+'''
+
+# Conexión a la base de datos (ajustar URL)
+DATABASE_URL = "postgresql://postgres:admin@localhost:5432/clivet"
+engine = create_engine(DATABASE_URL)
+
+# Cargar metadatos sin definir modelos
+metadata = MetaData()
+metadata.reflect(bind=engine)  # Reflejar todas las tablas existentes
+
+app = FastAPI()
+    
+@app.get("/procesos")
+async def get_procesos():
+    procesos = Table("proceso", metadata, autoload_with=engine)
+    query = select(procesos)
+    with engine.connect() as connection:
+        print('hola')
+        result = connection.execute(query)
+        rows = result.fetchall()
+
+    return [dict(row._mapping) for row in rows]
+
+
+
+@app.get("/procesos/{id}")
+async def get_proceso_id(id: int):
+    procesos = Table("proceso", metadata, autoload_with=engine)
+    query = select(procesos).where(procesos.c.id == id)
+    with engine.connect() as connection:
+        result = connection.execute(query)
+        rows = result.fetchall()
+
+    return [dict(row._mapping) for row in rows]
+    
