cf1388ded659a2db618e682600b08e225d1f917c
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2025-03-29 19:29:12 -0500

    ProfileCard created

--- CliVet_FrontEnd/src/views/client/ClientView.tsx
+++ CliVet_FrontEnd/src/views/client/ClientView.tsx
@@ -2,10 +2,11 @@ import Sidebar from "@/components/Sidebar";
 import { useState } from "react";
 import { FiLogOut } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import ProfileCard from "./ProfileCard";
 
 export default function ClientView() {
-    const [activeTab, setActiveTab] = useState("Servicios");
-    const [isSidebarOpen, setSidebarOpen] = useState(true);
+    const [activeTab, setActiveTab] = useState("Servicios"); // Estado de los tabs
+    const [isSidebarOpen, setSidebarOpen] = useState(true); // Estado del sidebar
     return (
         <div className="flex h-screen">
             <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} isOpen={isSidebarOpen} toggleSidebar={() => setSidebarOpen(!isSidebarOpen)} />
@@ -18,18 +19,26 @@ export default function ClientView() {
                     </Link>
                 </div>
                 {activeTab === "Servicios" && (
-                    <div className="flex justify-end space-x-2 mt-4">
+                    <div className="flex justify-start space-x-2 mt-4">
                         <button className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400">Agregar cita</button>
                         <button className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400">Agregar guardería</button>
                     </div>
                 )}
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-6">
-                    {[...Array(6)].map((_, index) => (
-                        <div key={index} className="border p-4 bg-white rounded shadow-md">
-                            <h3 className="font-semibold border-b pb-2">Información</h3>
-                        </div>
-                    ))}
-                </div>
+                {activeTab === "Mascotas" && (
+                    <div className="flex justify-start mt-4">
+                        <button className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400">Agregar mascota</button>
+                    </div>
+                )}
+                {activeTab === "Perfil" && <ProfileCard />}
+                {activeTab !== "Perfil" && (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-6">
+                        {[...Array(6)].map((_, index) => (
+                            <div key={index} className="border p-4 bg-white rounded shadow-md">
+                                <h3 className="font-semibold border-b pb-2">Información</h3>
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     );
