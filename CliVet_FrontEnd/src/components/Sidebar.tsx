f1174b2331f9fe56c7cd587e364cc54dd2a75f9b
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2025-03-29 19:22:08 -0500

    Changed position of toggle sidebar botton

--- CliVet_FrontEnd/src/components/Sidebar.tsx
+++ CliVet_FrontEnd/src/components/Sidebar.tsx
@@ -4,14 +4,12 @@ import { Link } from "react-router-dom";
 
 export default function Sidebar({ activeTab, setActiveTab, isOpen, toggleSidebar }) {
     return (
-        <div className={`fixed md:relative ${isOpen ? "w-64" : "w-20"} bg-gray-200 h-full transition-all`}> 
-            <div className="flex justify-between items-center p-4">
-                <Link to={'/'} className="text-3xl font-bold text-blue-600">{isOpen ? "CliVet" : "CV"}</Link>
-                <button onClick={toggleSidebar} className="text-2xl">
-                    <FiMenu />
-                </button>
-            </div>
-            <nav className="flex flex-col p-4 space-y-2">
+        <div className={`fixed md:relative ${isOpen ? "w-64" : "w-20"} bg-gray-200 h-full transition-all flex flex-col items-center`}> 
+            <Link to={'/'} className="text-3xl font-bold text-blue-600 mt-4">{isOpen ? "CliVet" : "CV"}</Link>
+            <button onClick={toggleSidebar} className="text-2xl mt-4">
+                <FiMenu />
+            </button>
+            <nav className="flex flex-col p-4 space-y-2 w-full">
                 {[{ name: "Servicios", icon: <FiClipboard /> }, { name: "Mascotas", icon: <FaPaw /> }, { name: "Perfil", icon: <FiUser /> }].map((tab) => (
                     <button
                         key={tab.name}
