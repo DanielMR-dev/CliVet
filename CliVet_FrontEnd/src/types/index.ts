ce75965bcea1024c6e1f7f98563252b476cde737
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2025-03-31 10:04:14 -0500

    Se crea el Esquema base para todos los usuarios

--- CliVet_FrontEnd/src/types/index.ts
+++ CliVet_FrontEnd/src/types/index.ts
@@ -2,3 +2,12 @@ import { z } from "zod";
 
 // Enum para los roles
 export const UserRoleEnum = z.enum(["ADMIN", "MEDICO", "CLIENTE"]);
+
+// Esquema base para todos los usuarios
+export const UsuarioSchema = z.object({
+    id: z.number(),
+    nombre: z.string().min(3, "El nombre debe tener al menos 3 caracteres"),
+    email: z.string().email("Correo electrónico inválido"),
+    telefono: z.string().optional(), // Puede ser opcional
+    role: UserRoleEnum,
+});
